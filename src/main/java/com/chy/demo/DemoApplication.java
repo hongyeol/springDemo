5388403ae15fed3228265f4f78a56d48c6e5e6fc
hongyeol chae <hoisn12@gmail.com>
2018-11-03 01:42:26 +0900

    test

--- src/main/java/com/chy/demo/DemoApplication.java
+++ src/main/java/com/chy/demo/DemoApplication.java
@@ -3,10 +3,14 @@ package com.chy.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	private void test(){
 	}
 }
